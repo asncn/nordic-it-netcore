e48686f56a631a01ecd02450ceea5ea3f239464d
asncn <asncn@mail.ru>
2020-10-02 19:22:32 +0300

    Class work of the 1st lesson added

---
+++ 01/HomeWork/ConsoleApp1/DemoApplication.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main()
+		{
+			Console.WriteLine("Enter name");
+			string name = Console.ReadLine();
+			Thread.Sleep(5000);
+			Console.WriteLine($"Hello, {name}!");
+			Thread.Sleep(5000);
+			Console.WriteLine($"Good bye, {name}!");
+			Console.ReadKey();
+		}
+	}
+}
