03a286d7b3b07af440dabc4c9a9a1f6d3d3d640d
asncn <asncn@mail.ru>
2020-10-03 18:01:29 +0300

    Home work ot the second lesson added

---
+++ 02/HomeWork/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите первое число");
+            string inputNumber1 = Console.ReadLine();
+            int number1 = int.Parse(inputNumber1);
+
+            Console.WriteLine("Введите второе число");
+            string inputNumber2 = Console.ReadLine();
+            int number2 = int.Parse(inputNumber2);
+            Console.WriteLine($"Сумма ваших чисел: {number1 + number2 }");
+            Console.WriteLine($"Разница ваших чисел: {number1 - number2 }");
+            Console.WriteLine($"Произведение ваших чисел: {number1 * number2 }");
+
+        }
+    }
+}
