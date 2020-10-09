7dc8a6820b2422892ad1000e7ad8c78d8076ca29
asncn <asncn@mail.ru>
2020-10-09 20:03:15 +0300

    Home work of the 3rd lesson is added

--- 02/HomeWork/ConsoleApp1/Program.cs
+++ 02/HomeWork/ConsoleApp1/Program.cs
@@ -13,10 +13,33 @@ namespace ConsoleApp1
             Console.WriteLine("Введите второе число");
             string inputNumber2 = Console.ReadLine();
             int number2 = int.Parse(inputNumber2);
-            Console.WriteLine($"Сумма ваших чисел: {number1 + number2 }");
-            Console.WriteLine($"Разница ваших чисел: {number1 - number2 }");
-            Console.WriteLine($"Произведение ваших чисел: {number1 * number2 }");
 
+            Console.WriteLine("выберите одно из: +, -, *, :, /, ^n");
+            string inputop = Console.ReadLine();
+            char inputop1 = char.Parse(inputop);
+            if (inputop = '+')
+            {
+                Console.WriteLine($"Сумма ваших чисел: {number1 + number2 }");
+            }
+            else if (inputop1 = '-')
+            {
+                Console.WriteLine($"Разница ваших чисел: {number1 - number2 }");
+            }
+            else if (Condition3)
+            {
+                if (inputop1 = ':')
+                {
+                    Console.WriteLine($"Произведение ваших чисел: {number1 * number2 }");
+                }
+                else
+                {
+                    // Condition1, Condition2, and Condition4 are false. Condition3 is true.
+                }
+            }
+            else
+            {
+                // Condition1, Condition2, and Condition3 are false.
+            }
         }
     }
 }
---
+++ 03/HomeWork/ConsoleApp2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+           
+            int[] table1 = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] table2 = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            ConsoleColor fgcolor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+
+            Console.Write("   *");
+
+            for (table1[0] =1; table1[0] <=table1.Length; table1[0]++)
+            Console.Write("{0, 4}", table1[0]);
+
+            Console.ForegroundColor = fgcolor;
+            Console.WriteLine();
+                                  
+            for (table1[0] = 1; table1[0] <= table1.Length; table1[0]++)
+
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.Write("{0, 4}", table1[0]);
+
+                for (table2[0] = 1; table2[0] <= table2.Length; table2[0]++)
+                    
+                {
+                    Console.ForegroundColor = fgcolor;
+                    Console.Write("{0, 4}", table1[0] * table2[0]);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
