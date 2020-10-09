19b5b247380797a7ff25f34506f27406eae8c00d
asncn <asncn@mail.ru>
2020-10-09 20:12:53 +0300

    Home work of the 3rd lesson is added

--- 03/HomeWork/ConsoleApp2/Program.cs
+++ 03/HomeWork/ConsoleApp2/Program.cs
@@ -5,7 +5,6 @@ namespace ConsoleApp2
     class Program
     {
         static void Main(string[] args)
-
         {
            
             int[] table1 = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
@@ -23,19 +22,18 @@ namespace ConsoleApp2
             Console.WriteLine();
                                   
             for (table1[0] = 1; table1[0] <= table1.Length; table1[0]++)
-
             {
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
                 Console.Write("{0, 4}", table1[0]);
 
                 for (table2[0] = 1; table2[0] <= table2.Length; table2[0]++)
-                    
                 {
                     Console.ForegroundColor = fgcolor;
                     Console.Write("{0, 4}", table1[0] * table2[0]);
                 }
                 Console.WriteLine();
             }
+
             Console.ReadLine();
         }
     }
