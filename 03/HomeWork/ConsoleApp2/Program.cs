79761a9279744ee1f51245e56bfe1d65076f0b41
asncn <asncn@mail.ru>
2020-10-14 17:40:40 +0300

    Home work of the 4th lesson added

--- 03/HomeWork/ConsoleApp2/Program.cs
+++ 03/HomeWork/ConsoleApp2/Program.cs
@@ -6,21 +6,20 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-           
+
             int[] table1 = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             int[] table2 = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
             ConsoleColor fgcolor = Console.ForegroundColor;
             Console.ForegroundColor = ConsoleColor.DarkYellow;
-
+                       
             Console.Write("   *");
 
-            for (table1[0] =1; table1[0] <=table1.Length; table1[0]++)
-            Console.Write("{0, 4}", table1[0]);
+            for (table1[0] = 1; table1[0] <= table1.Length; table1[0]++)
+                Console.Write("{0, 4}", table1[0]);
 
             Console.ForegroundColor = fgcolor;
             Console.WriteLine();
-                                  
+
             for (table1[0] = 1; table1[0] <= table1.Length; table1[0]++)
             {
                 Console.ForegroundColor = ConsoleColor.DarkYellow;
@@ -37,4 +36,3 @@ namespace ConsoleApp2
             Console.ReadLine();
         }
     }
-}
---
+++ 04/HomeWork/ConsoleApp1/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+using System.Globalization;
+
+
+
+namespace _4th_Homework
+
+{
+
+    enum containersType
+
+    {
+
+        Small = 1,
+        Medium = 5,
+        Large = 20
+
+    }
+
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            int firstContainer = 0;
+            int secondContainer = 0;
+            int thirdContainer = 0;
+
+            string delimiter =
+
+                CultureInfo
+                .CurrentCulture
+                .NumberFormat
+                .NumberDecimalSeparator;
+
+
+
+            Console.WriteLine("Какой объем сока (в литрах) требуется упаковать? " +
+                $"Введите число, используя '{delimiter}' ,если оно не целое");
+
+            string InputNumber = Console.ReadLine();
+
+            double Number = double.Parse(InputNumber);
+
+            Console.WriteLine("Вам потребуются следующие контейнеры:");
+            for (thirdContainer = 0; (double)containersType.Large <= Number; thirdContainer++)
+            {
+
+                Number = Math.Ceiling(Number - (double)containersType.Large);
+
+            }
+
+            if (thirdContainer != 0)
+            {
+
+                Console.WriteLine($"20л: " + thirdContainer + "шт.");
+
+            }
+
+            for (secondContainer = 0; (double)containersType.Medium <= Number; secondContainer++)
+            {
+
+                Number = Math.Ceiling(Number - (double)containersType.Medium);
+
+            }
+
+            if (secondContainer != 0)
+            {
+
+                Console.WriteLine($"5л: " + secondContainer + "шт.");
+
+            }
+
+            for (firstContainer = 0; (double)containersType.Small <= Number; firstContainer++)
+            {
+
+                Number = Math.Ceiling(Number - (double)containersType.Small);
+
+            }
+
+            if (firstContainer != 0)
+            {
+
+                Console.WriteLine($"1л: " + firstContainer + "шт.");
+
+            }
+
+        }
+
+    }
+
+}
+
+
