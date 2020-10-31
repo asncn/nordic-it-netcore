70df24504645a83d219273e1075a2b4c71ab3995
asncn <asncn@mail.ru>
2020-10-31 06:34:25 +0300

    Home work № 2 of the 7th lesson added

---
+++ 07/HomeWork/02/ConsoleApp07-2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+namespace Homework_2_of7th_lesson
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string textInput;
+			Console.WriteLine("Введите текст: ");
+			do
+			{
+				textInput  = Console.ReadLine();
+				if (string.IsNullOrWhiteSpace(textInput))
+				{
+					Console.WriteLine("Вы ввели пустую строку! Попробуйте ещё раз: ");
+					continue;
+				}
+				StringBuilder lineNew = new StringBuilder(textInput.Length);
+				int i = 0;
+
+				for (; i < lineNew.Capacity; i++)
+					lineNew.Insert(0, textInput[i]);
+			
+				Console.WriteLine(lineNew.ToString().ToLower());
+
+			} while (string.IsNullOrWhiteSpace(textInput));
+		}
+	}
+}
