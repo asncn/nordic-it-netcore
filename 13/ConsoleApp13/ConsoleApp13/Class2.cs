965832fa8272766e568c64fbf9fb7c7d8edccba5
asncn <asncn@mail.ru>
2020-11-18 22:11:53 +0300

    Class work added (lesson #12)

---
+++ 13/ConsoleApp13/ConsoleApp13/Class2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp13
+{
+    class ConsoleWiter : IWriter
+    {
+        public void Dispose()
+        {
+            Console.WriteLine("освобождение ресурсов");
+        }
+
+        public void Write(string content)
+        {
+            Console.WriteLine(content);                              ;
+        }
+    }
+}
---
+++ 13/ConsoleApp13/ConsoleApp13/IPropertiesWriter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp13
+{
+    public interface IPropertiesWriter
+    {
+        void WriteAllProperties();
+    }
+}
