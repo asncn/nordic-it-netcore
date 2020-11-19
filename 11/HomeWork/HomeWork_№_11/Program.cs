169c62e9a1bc04c8c1492f039c17d628b7cdf6ef
asncn <asncn@mail.ru>
2020-11-19 15:53:01 +0300

    Home work № 11 is added

--- 11/HomeWork/HomeWork_№_11/Program.cs
+++ 11/HomeWork/HomeWork_№_11/Program.cs
@@ -1,18 +1,14 @@
-﻿using System;
-namespace HomeWork___11
+﻿namespace HomeWork___11
 {
 	class Program
 	{
 		static void Main(string[] args)
-		{
-			
-			ReminderItem[] reminders = new ReminderItem[2];
-			
+		{			
+			ReminderItem[] reminders = new ReminderItem[2];			
 			for (int i = 0; i < 2; i++)
 			{
 				reminders[i].WriteProperties();
 			}
-
 		}
 	}
 }
