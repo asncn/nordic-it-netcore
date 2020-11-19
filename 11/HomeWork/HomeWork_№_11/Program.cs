7ba73bc050243f0ff11198392a21c33a015a182a
asncn <asncn@mail.ru>
2020-11-19 19:28:10 +0300

    Home work № 11 is updated

--- 11/HomeWork/HomeWork_№_11/Program.cs
+++ 11/HomeWork/HomeWork_№_11/Program.cs
@@ -9,7 +9,7 @@ namespace HomeWork___11
 			DateTimeOffset alarmDate = default;
 			string alarmMessage = null;
 			ReminderItem[] reminders = new ReminderItem[2];
-            reminders[0] = new ReminderItem(alarmDate, alarmMessage);
+			reminders[0] = new ReminderItem(alarmDate, alarmMessage);
 			reminders[1] = new ReminderItem(alarmDate, alarmMessage);
 
 			for (int i = 0; i < 2; i++)
