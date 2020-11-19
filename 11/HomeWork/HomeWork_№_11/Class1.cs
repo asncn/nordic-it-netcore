6982522c4c0ec619eb8b520f942effe998e53a4a
asncn <asncn@mail.ru>
2020-11-19 15:48:09 +0300

    Home work № 11 is added

---
+++ 11/HomeWork/HomeWork_№_11/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HomeWork___11
+{
+	class ReminderItem
+	{
+		public DateTimeOffset AlarmDate { get; set; }
+		public string AlarmMessage { get; set; }
+		public TimeSpan TimeToAlarm
+		{
+			get
+			{
+				return (AlarmDate - DateTimeOffset.Now);
+			}
+		}
+		public bool IsOutdated
+		{
+			get
+			{
+				return TimeToAlarm >= TimeSpan.Zero;
+			}
+		}
+		public ReminderItem(DateTimeOffset alarmDate, string alarmMessage)
+		{
+
+			AlarmDate = alarmDate;
+			AlarmMessage = alarmMessage;
+		}
+		public void WriteProperties()
+		{
+			string outDated = IsOutdated
+				? "будильник не просрочен"
+				: "будильник просрочен";
+			Console.WriteLine($"Дата и время будильника: {AlarmDate}, " +
+				$" текст будильника: {AlarmMessage}, " +
+				$"\nвремя до срабатывания будильника: {TimeToAlarm}, {outDated}");
+		}
+	}
+}
+
+
+
---
+++ 11/HomeWork/HomeWork_№_11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace HomeWork___11
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			ReminderItem[] reminders = new ReminderItem[2];
+			
+			for (int i = 0; i < 2; i++)
+			{
+				reminders[i].WriteProperties();
+			}
+
+		}
+	}
+}
